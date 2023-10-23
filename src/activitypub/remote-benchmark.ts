6d0801289d6c3ef3672e54c0502215df2211025a
Nazim Lachter <nazim@blueforest.cc>
2023-10-23 22:24:32 +0200

    benchmark

---
+++ src/activitypub/remote-benchmark.ts
@@ -0,0 +1,28 @@
+import { EXT } from "./values.ts"
+
+const files = await getFile("") as string[]
+
+let now = Date.now()
+await Promise.all(files.map(async (file, i) => {
+  await getFile(file)
+  //console.log("#", i + "/" + files.length, totalTime(now))
+}))
+console.log("#", files.length, "files in", totalTime(now), "seconds without LFS")
+
+now = Date.now()
+await Promise.all(files.map(file => getFile(file, true)))
+console.log("#", files.length, "files in", totalTime(now), "seconds with LFS")
+
+function totalTime(start: number) {
+  return (Date.now() - start) / 1000
+}
+
+async function getFile(name: string, lfs = false) {
+  const url = lfs
+    ? ("https://media.githubusercontent.com/media/n4zim/.mod.pm/lfs/data/activitypub/" + name + EXT + "?download=")
+    : ("https://raw.githubusercontent.com/n4zim/.mod.pm/main/data/activitypub/" + name + EXT)
+  //console.log("#", url)
+  const response = await fetch(url)
+  if(!response.ok) return
+  return response.json()
+}
